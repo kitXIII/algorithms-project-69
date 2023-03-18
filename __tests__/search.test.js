5e5d0696c17cb8c2862de57a5e126f4a91312742
Nikita Konovalov <mipo4t@gmail.com>
2023-03-18 15:37:01 +0400

    add search function

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -1,7 +1,28 @@
+import { describe, expect, test } from '@jest/globals';
 import search from '../src/index.js';
 
-describe('Search. Basic.', () => {
-  test('check', () => {
-    expect(search()).toBeNull();
+describe('Search', () => {
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+  const doc3 = { id: 'doc3', text: "I'm your shooter." };
+
+  const docs = [doc1, doc2, doc3];
+  test('Find by word', () => {
+    const result = search(docs, 'shoot');
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual(expect.arrayContaining([doc1.id, doc2.id]));
+  });
+
+  test('Returns empty array if no docs', () => {
+    const result = search([], 'shoot');
+
+    expect(result).toHaveLength(0);
+  });
+
+  test('Returns empty array if no matches', () => {
+    const result = search(docs, 'nomatches');
+
+    expect(result).toHaveLength(0);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const search = () => null;
+const search = (docs, sample) => docs.reduce((acc, doc) => {
+  const words = doc.text.split(' ');
+
+  if (words.some((w) => w === sample)) {
+    return [...acc, doc.id];
+  }
+
+  return acc;
+}, []);
 
 export default search;
